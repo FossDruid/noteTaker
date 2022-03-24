c70e776b0bb4632806e1f538d0cb921b42f97284
LunchTimeDoublySo <massoh7238@gmail.com>
2022-03-24 23:32:23 +0100

    Removed full path, using relative path to sqlite

--- noteTaker/Models/DataContext.cs
+++ noteTaker/Models/DataContext.cs
@@ -7,6 +7,6 @@ namespace noteTaker.Models
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
         public DbSet<note> notes { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite(@"Data Source=C:\Users\coolDevGuy\Documents\noteTakerUtivklerOppgave2\noteTaker\noteTaker\UserNotes.db");
+            => options.UseSqlite(@"Data Source=UserNotes.db");
     }
 }
